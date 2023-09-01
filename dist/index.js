352f0e9661f8f3dfc2aa398d3197e1aafa0e3358
Aandreba <aandrebafreelancer@gmail.com>
2023-09-01 22:04:16 +0200

    include modules to github

---
+++ dist/index.js
@@ -0,0 +1,111 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+const _core = /*#__PURE__*/ _interop_require_wildcard(require("@actions/core"));
+const _github = /*#__PURE__*/ _interop_require_wildcard(require("@actions/github"));
+const _toolcache = /*#__PURE__*/ _interop_require_wildcard(require("@actions/tool-cache"));
+const _nodeassert = /*#__PURE__*/ _interop_require_wildcard(require("node:assert"));
+function _getRequireWildcardCache(nodeInterop) {
+    if (typeof WeakMap !== "function") return null;
+    var cacheBabelInterop = new WeakMap();
+    var cacheNodeInterop = new WeakMap();
+    return (_getRequireWildcardCache = function(nodeInterop) {
+        return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+    })(nodeInterop);
+}
+function _interop_require_wildcard(obj, nodeInterop) {
+    if (!nodeInterop && obj && obj.__esModule) {
+        return obj;
+    }
+    if (obj === null || typeof obj !== "object" && typeof obj !== "function") {
+        return {
+            default: obj
+        };
+    }
+    var cache = _getRequireWildcardCache(nodeInterop);
+    if (cache && cache.has(obj)) {
+        return cache.get(obj);
+    }
+    var newObj = {};
+    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for(var key in obj){
+        if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+            if (desc && (desc.get || desc.set)) {
+                Object.defineProperty(newObj, key, desc);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    newObj.default = obj;
+    if (cache) {
+        cache.set(obj, newObj);
+    }
+    return newObj;
+}
+async function main() {
+    brk: try {
+        const octo = _github.getOctokit(await _core.getIDToken());
+        let tag = _core.getInput("version");
+        const release = await (tag.length === 0 || tag === "latest" ? octo.rest.repos.getLatestRelease({
+            owner: "WebAssembly",
+            repo: "binary"
+        }) : octo.rest.repos.getReleaseByTag({
+            owner: "WebAssembly",
+            repo: "binary",
+            tag: `version_${tag}`
+        }));
+        _nodeassert.match(release.data.tag_name, /^version\_/g);
+        const version = release.data.tag_name.substring(8);
+        let os;
+        switch(process.platform){
+            case "linux":
+                os = "linux";
+                break;
+            case "darwin":
+                os = "macos";
+                break;
+            case "win32":
+            case "cygwin":
+                os = "windows";
+                break;
+            default:
+                os = process.platform;
+                _core.warning(`unknown platform "${process.platform}"`);
+        }
+        let arch;
+        switch(process.arch){
+            case "arm64":
+                arch = "arm64";
+                break;
+            case "x64":
+                arch = "x86_64";
+                break;
+            default:
+                arch = process.arch;
+                _core.warning(`unknown architecture "${process.arch}"`);
+        }
+        const target = `${arch}-${os}`;
+        const end = `${target}.tar.gz`;
+        const cachedPath = _toolcache.find("binaryen", version, target);
+        if (cachedPath.length > 0) {
+            _core.debug("cached version of binaryen found");
+            _core.addPath(cachedPath);
+            break brk;
+        }
+        for (let asset of release.data.assets){
+            if (!asset.name.endsWith(end)) continue;
+            const tarball = await _toolcache.downloadTool(asset.browser_download_url);
+            const extracted = await _toolcache.extractTar(tarball);
+            const cached = await _toolcache.cacheDir(extracted, "binaryen", version, target);
+            _core.addPath(cached);
+            break brk;
+        }
+        throw new Error(`binaries for target "${target}" not found`);
+    } catch (e) {
+        _core.setFailed(e instanceof Error ? e : "unknown error kind");
+    }
+}
+main();
---
+++ justfile
@@ -0,0 +1,4 @@
+build:
+    rm -rf node_modules
+    npm ci
+    npm run build
